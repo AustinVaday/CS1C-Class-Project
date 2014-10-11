b0492cd8984a64012a932359a0195c10c18b561c
Austin Vaday <avaday0@ivc.edu>
2014-10-10 18:57:01 -0700

    set up this project with Visual Studio

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char argv[])
+{
+	cout << "hello world";
+
+
+
+
+	cin.ignore(1000, '\n');
+	return 0;
+}
+
